0d2db550cf3d791483ac11a270c2855484ed06a9
Shrijit_01 <shrijitpandey113@gmail.com>
2025-01-17 15:54:19 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
-import Header from './Components/Header';
-import Home from './Components/Home';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [Name, setName] = useState("LOGO");
+  const [title, setTitle] = useState("");
+
+document.title = title;
+  useEffect(() => {
+    
+    // Fetch data from the backend
+    fetch("http://localhost:5000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setTitle(data.title); // Set the title from the backend
+        // console.log(data)
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
-    <>
-      <Header user={Name}/>
-      <Home />
-    </>
+    <div>
+      <h1>{title ? title :"Loading"}</h1> {/* Display title or loading message */}
+      <>
+        {/* <Header user={Name}/> */}
+        {/* <Home /> */}
+      </>
+    </div>
   );
 }
 
